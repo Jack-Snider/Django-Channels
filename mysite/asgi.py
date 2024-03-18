c4ecdb1c4151a9dbe287935f665a13e39ba76a50
= <choiman2021@gmail.com>
2024-03-18 21:28:54 +0900

    mysite/asgi.py edited

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -11,6 +11,9 @@ import os
 
 from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
+from channels.routing import URLRouter
+
+import app.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
@@ -19,4 +22,7 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http" : django_asgi_app, 
+    "websocket" : URLRouter(
+        app.routing.websocket_urlpatterns
+    ),
 })
